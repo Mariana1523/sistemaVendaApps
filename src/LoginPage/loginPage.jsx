3f4c6aedc5f6f327fbaa89d29ff5b7cdf2b9027e
MontMor4 <victorperial@hotmail.com>
2023-06-19 21:46:16 -0300

    logo-login

--- src/LoginPage/loginPage.jsx
+++ src/LoginPage/loginPage.jsx
@@ -12,8 +12,8 @@ export default function loginPage() {
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
-              className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              className="mx-auto h-20 w-auto"
+              src="/imgs/Logo.png"
               alt="Your Company"
             />
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
@@ -25,7 +25,7 @@ export default function loginPage() {
             <form className="space-y-6" action="#" method="POST">
               <div>
                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email address
+                  Email
                 </label>
                 <div className="mt-2">
                   <input
@@ -42,11 +42,11 @@ export default function loginPage() {
               <div>
                 <div className="flex items-center justify-between">
                   <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Password
+                    Senha
                   </label>
                   <div className="text-sm">
                     <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                      Forgot password?
+                      Esqueceu a senha?
                     </a>
                   </div>
                 </div>
@@ -67,17 +67,17 @@ export default function loginPage() {
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Sign in
+                  Login
                 </button>
               </div>
             </form>
   
-            <p className="mt-10 text-center text-sm text-gray-500">
+            {/* <p className="mt-10 text-center text-sm text-gray-500">
               Not a member?{' '}
               <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
                 Start a 14 day free trial
               </a>
-            </p>
+            </p> */}
           </div>
         </div>
       </>
