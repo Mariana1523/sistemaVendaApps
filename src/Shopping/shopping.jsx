a9e8023e9f10f0033c07f481f4f8d268e1df00ca
Pedro R <rochapedro436@gmail.com>
2023-06-20 22:02:32 -0300

    Ajustes escala hover css

--- src/Shopping/shopping.jsx
+++ src/Shopping/shopping.jsx
@@ -81,10 +81,10 @@ const products = [
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
               <h2 className="text-2xl font-bold tracking-tight text-gray-900">Aplicativos disponíveis</h2>
       
-              <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 pointer">
+              <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 ">
                 {products.map((product) => (
                   <div key={product.id} onClick={() => setPopupOpen(true)}>
-                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                    <div className="aspect-h-1 pointer aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                       <img
                         src={product.imageSrc}
                         alt={product.imageAlt}
