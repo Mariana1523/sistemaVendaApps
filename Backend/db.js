d40bef8ca976e72d919809626450c8db6cd02c55
Pedro R <rochapedro436@gmail.com>
2023-06-21 23:05:32 -0300

    Inicio Banco de dados

---
+++ Backend/db.js
@@ -0,0 +1,18 @@
+const Client = require('pg').Client
+const cliente = new Client({
+   user:"postgres",
+   password:"admin_pr",
+   host:"127.0.0.1",
+   port:5432,
+   database: "Sistema"
+})
+ 
+
+cliente.connect()
+
+cliente.query("select * from usuario")
+.then(results =>{
+   const resultado = results.rows
+   console.log(resultado)
+})
+.finally(()=> cliente.end())
