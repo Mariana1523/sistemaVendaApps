250cacada85b378fca779a36c90cbae6cec57721
Pedro R <rochapedro436@gmail.com>
2023-06-23 22:55:41 -0300

    Ajustes

--- Front/src/Shopping/shopping.jsx
+++ Front/src/Shopping/shopping.jsx
@@ -38,46 +38,8 @@ function classNames(...classes) {
 }
 
 
-const products = [
-    {
-      id: 1,
-      name: 'Basic Tee',
-      href: '#',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-      imageAlt: "Front of men's Basic Tee in black.",
-      price: '$35',
-      color: 'Black',
-    },
-    {
-        id: 2,
-        name: 'Nomad Tumbler',
-        href: '#',
-        price: '$35',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-        imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
-      },
-      {
-        id: 3,
-        name: 'Focus Paper Refill',
-        href: '#',
-        price: '$89',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
-        imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
-      },
-      {
-        id: 4,
-        name: 'Machined Mechanical Pencil',
-        href: '#',
-        price: '$35',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
-        imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
-      },
-  ]
   
   export default function ShoppingPage() {
-
-       
-
         useEffect(() => {
           getAplicativos();
         }, []);
@@ -127,7 +89,6 @@ const products = [
                 ))}
               </div>
             </div>
-      
             {isPopupOpen && (
               <div className="fixed inset-0 flex items-center justify-center">
                 <div className="bg-white p-8 rounded-lg">
